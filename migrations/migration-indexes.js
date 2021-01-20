afc6f9f403422c37e4f27029b78b0dd2bf0e6419
narteysarso <narteysarso@gmail.com>
2021-01-20 22:54:49 +0000

    Add list of supported tables indexing

---
+++ migrations/migration-indexes.js
@@ -0,0 +1,9 @@
+
+const MIGRATION_INDEXES = {
+    "index": "index",
+    "spatialIndex": "spatialIndex",
+    "unique": "unique",
+    "primary": "primary"
+}
+
+module.exports = MIGRATION_INDEXES;
